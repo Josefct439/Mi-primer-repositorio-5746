0a65b65156d69a1e6de774d5203528d72c3a3821
José Cabarcas <132719140+Josefct439@users.noreply.github.com>
2025-02-27 18:10:57 -0500

    Update generador.py

--- generador.py
+++ generador.py
@@ -1,11 +1,11 @@
 import random
 
 caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-longitud = int(input("Ingresa la cantidad de caracteres: "))
-password = ""
-
-for i in range(longitud):
-    caracter = random.choice(caracteres)
-    password = password + caracter
-
-print("Esta es tu password: " + password)
+def gen_pass(longitud):
+    password = ""
+    
+    for i in range(longitud):
+        caracter = random.choice(caracteres)
+        password = password + caracter
+    
+    return("Esta es tu password: " + password)
